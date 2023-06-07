6bfd5a40878405e05ced3a0d512125f882c485a9
Matías Godoy <36138248+msebastianlupo@users.noreply.github.com>
2023-06-07 02:28:58 -0300

    testeo: promesa en service worker

--- js/sw-register.js
+++ js/sw-register.js
@@ -3,7 +3,7 @@
 if('serviceWorker' in navigator){
     navigator.serviceWorker.register('../sw.js')
     .then((mensaje) => {
-        console.info(mensaje);
+        console.info("Service Worker ok");
     })
 }else{
     console.warn("Este navegador no soporta Service Worker");
