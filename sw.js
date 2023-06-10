68716f99779416540d9f28df920d19f8ac1cdfee
Matías Godoy <36138248+msebastianlupo@users.noreply.github.com>
2023-06-10 02:18:34 -0300

    testeo: intentando precachear recursos

--- sw.js
+++ sw.js
@@ -10,15 +10,29 @@ const precacheados = [
     '/index.html',
     '/guardadas.html',
     '/trailers.html',
-    '/css-ft/estilos.css'
+    '/css-ft/estilos.css',
+    '/css-ft/default-sans.ttf',
+    '/img/busqueda.png',
+    '/img/cerrado.png',
+    '/img/guardado.png',
+    '/img/loading.png',
+    '/img/logo.png',
+    '/img/menu.png',
+    '/img/no-disponible.png',
+    '/img/siguiente.png',
+    '/js/accion.js',
+    '/js/datos.js',
+    '/js/principal.js',
+    '/js/youtube.js'
 ];
   
-  self.addEventListener('install', (event) => {
-    event.waitUntil(caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(precacheados);
-    }));
-  });       
-
+self.addEventListener('install', (event) => {
+    event.waitUntil(caches.open(CACHE_NAME)
+        .then((cache) => {
+            return cache.addAll(precacheados);
+        })
+    );
+});       
 
 self.addEventListener("message", event => {
     if(event.data && event.data.type == "SKIP_WAITING"){
